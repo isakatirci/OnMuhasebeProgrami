e25d5edaba26f88c2d6a3d0e8ccb8b51d04f2ebd
isakatirci <ce.isakatirci@gmail.com>
2020-05-24 18:21:19 +0300

    *

---
+++ OnMuhasebeProgrami.WebUI/Controllers/FaturaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnMuhasebeProgrami.WebUI.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnMuhasebeProgrami.WebUI/ViewModels/FaturaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnMuhasebeProgrami.WebUI.ViewModels
+{
+    public class FaturaViewModel
+    {
+    }
+}
